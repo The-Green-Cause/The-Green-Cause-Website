926b89371d2e08c0d496d3b131dd89f24b06e7a9
monccciii <sook361@yahoo.com>
2023-04-12 15:43:47 -0400

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <Navbar/>
         <div id='imagebg' className='flex justify-center items-center bg-green-900 w-full h-[80vh]'>
           <div className="text-center">
-            <p className='text-white text-2xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl'>Education for our nation, with the hope of spreading awareness about the world's environmental concerns</p>
+            <p className='text-white text-2xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl'>Education for our nation, with the hope of spreading awareness about the world&apos;s environmental concerns</p>
             <button className='mt-10 text-black font-bold bg-white py-2 px-5 rounded'>Join Us!</button>
           </div>
         <div id='3grid'>
